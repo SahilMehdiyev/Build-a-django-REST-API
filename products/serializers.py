64b2711922341160f0e09c0c721e2843d81f1a58
SahilMehdiyev <branchsahil@gmail.com>
2024-11-27 22:43:58 +0400

    adding permissions and staff user

--- products/serializers.py
+++ products/serializers.py
@@ -1,18 +1,27 @@
 from rest_framework import serializers
 from .models import Product
+from rest_framework import reverse
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    # url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields = [
-            
+            'pk',
+            # 'url',
             'title',
             'content',
             'price',
             'sale_price', 
             'my_discount',  # Ensure this field is included
         ]
+        
+    # def get_url(self,obj):
+    #     request = self.context.get('request')
+    #     return reverse('product-detail', kwargs={'pk': obj.pk}, request=request)
+        # return f'/api/products/{obj.pk}/'
+    
         
     def get_my_discount(self,obj):
         if not hasattr(obj,'id'):
--- products/views.py
+++ products/views.py
@@ -1,25 +1,25 @@
 from django.http import Http404
-from rest_framework import generics,mixins
+from rest_framework import generics,mixins,permissions,authentication
 from .models import Product
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_list_or_404
 from.serializers import ProductSerializer
+from .permissions import IsStaffEditorPermission
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [IsStaffEditorPermission]
     
     def perform_create(self,serializer):
-        print(serializer.validated_data)
-        
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content')
         
         if content is None:     
             content = title
-        
         serializer.save(content=content)
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
